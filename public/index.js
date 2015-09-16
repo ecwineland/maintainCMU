0cea47f385ad9ea7604d1579bfd4dd7d18855f23
Evan <ewinelan@andrew.cmu.edu>
2015-09-16 11:11:33 -0400

    Added local array storage for requests.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
-# stuff
+$(function() {
+	$('#submit').click(function(e) {
+		e.preventDefault();
+		var building = $('#building').val();
+		var roomNum = $('#roomNum').val();
+		var floorNum = $('#floorNum').val();
+		var location = $('#location').val();
+		var description = $('#description').val();
+		// var photo = $('#')
+		console.log('here');
+		$.ajax({
+			url: 'song/'+title+'/'+album+'/'+artist,
+			type: 'PUT',
+			success: function(result) {
+				$('#exRequests').html(JSON.stringify(result));
+			}
+		})
+		return false;
+	})
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,19 @@
-var mongo = require("../models/request.js")
+// var mongo = require("../models/request.js")
 
-exports.index = function(req, res) {
-  res.render('index')
-};
+// exports.index = function(req, res) {
+//   res.render('index')
+// };
 
-exports.mongo = function(req, res){ // insert the only needed operation
-	console.log("req.query is "+JSON.stringify(req.query));
-    mongo.insert( 'recommendations', // collection
-                  req.query,
-                  function(model) { 
-                    res.render('mongo', {title: 'Mongo Demo', obj: model});
-                    }
-                  );
-  console.log("at end of insert case");
-}
+// exports.mongo = function(req, res){ // insert the only needed operation
+// 	console.log("req.query is "+JSON.stringify(req.query));
+//     mongo.insert( 'recommendations', // collection
+//                   req.query,
+//                   function(model) { 
+//                     res.render('mongo', {title: 'Mongo Demo', obj: model});
+//                     }
+//                   );
+//   console.log("at end of insert case");
+// }
 
 	// In the case that no route has been matched
 exports.errorMessage = function(req, res){
@@ -21,3 +21,14 @@ exports.errorMessage = function(req, res){
 	// Set the status to 404 not found, and render a message to the user.
   res.status(404).send(message);
 };
+
+var request = require('../models/music.js')
+
+exports.addRequest = function(req, res) {
+  console.log("Creating request for building: "+req.params.building+" on floor: "+req.params.floornum+" in room number: "+req.params.roomnum+" with location description: "+req.params.loc+" with problem description: "+req.params.descr);
+  var requests = request.addRequest(req.params.title,req.params.album,req.params.artist).length;
+  res.render('add', {title: req.params.title,
+             album: req.params.album,
+             artist: req.params.artist,
+             count: count});
+}
